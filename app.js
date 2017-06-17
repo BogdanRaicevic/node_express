61b3697fa0e49620527b4bf4814ff5be9acdd530
BogdanRaicevic <raicevic.bogdan@gmail.com>
2017-06-17 15:54:32 +0200

    bootstrap theme, routes set

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 var express = require('express');
 
 var app = express();
+
 var port = 3000;
-var host = 'localhost'
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+
+app.get('/', function(req,res){
+    res.send("hello world");
+});
+
+app.get('/books', function(req,res){
+    res.send("hello books");
+});
+
+
 app.listen(port,function(err){
     console.log('running server on port: '+port);
 })
