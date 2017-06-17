6c49b35088c9458bbfc6262d954b74179cded6bd
BogdanRaicevic <raicevic.bogdan@gmail.com>
2017-06-17 20:02:35 +0200

    gulp style set up successfully

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ app.use(express.static('src/views'));
 
 
 app.get('/', function(req,res){
-    res.send("hello world");
+    res.send('hello world');
 });
 
 app.get('/books', function(req,res){
-    res.send("hello books");
+    res.send('hello books');
 });
 
 
 app.listen(port,function(err){
     console.log('running server on port: '+port);
-})
+});
